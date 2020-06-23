1bec1f5730877bff0de132519819a1f37d8f7c54
imagineapril <64987202+imagineapril@users.noreply.github.com>
2020-06-24 02:10:29 +0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,6 @@
+package ru.timokhina.lesson9.task01;
+
+public abstract class Animal {
+
+    public abstract String getName();
+}
---
+++ Unicorn.java
@@ -0,0 +1,9 @@
+package ru.timokhina.lesson9.task01;
+
+public class Unicorn extends Animal {
+
+    @Override
+    public String getName() {
+        return "Unicorn";
+    }
+}
